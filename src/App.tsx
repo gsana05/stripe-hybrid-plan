d99ac21e2207267fcab6b74535dadc4c8859868e
sanas <gareth.sanashee@apptaura.com>
2022-11-22 00:09:32 -0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import getStripe from "../src/stripe/Stripe"
+
+import Checkout from "./components/Checkout";
+import Success from "./components/Success";
+import Cancel from "./components/Cancel";
+
+import "@stripe/stripe-js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route index element={<Checkout />} />
+          <Route path="success" element={<Success />} />
+          <Route path="cancel" element={<Cancel />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Checkout.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+import CardIcon from "../images/credit-card.jpg";
+import ProductImage from "../images/product-image.jpg";
+
+import "../styles.css";
+
+import getStripe from "../../src/stripe/Stripe"
+
+const Checkout = () => {
+  const [stripeError, setStripeError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  const redirectToCheckout = async () => {
+    setLoading(true);
+    console.log("redirectToCheckout");
+
+    const stripe = await getStripe();
+    if(stripe !== null){
+      //const { error } = await stripe.redirectToCheckout(checkoutOptions);
+
+      const { error } = await stripe.redirectToCheckout({
+        lineItems: [
+          {
+            price: "price_1M6qTBHLz4JOaSbM34Yn4Uiw",
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        successUrl: `${window.location.origin}/success`,
+        cancelUrl: `${window.location.origin}/cancel`,
+        customerEmail: 'customer@email.com',
+      });
+
+      console.log("Stripe checkout error", error);
+
+      if (error) {
+        setStripeError(error.message ? error.message : "ERROR");  
+      }
+
+      setLoading(false);
+
+    }
+    
+  };
+
+  if (stripeError) alert(stripeError);
+
+  return (
+    <div className="checkout">
+      <h1>HYBRID ATHLETE PLAN</h1>
+      <h1 className="checkout-price">$19</h1>
+      <img
+        className="checkout-product-image"
+        src={ProductImage}
+        alt="Product"
+      />
+      <button
+        className="checkout-button"
+        onClick={redirectToCheckout}
+        disabled={isLoading}
+      >
+        <div className="grey-circle">
+          <div className="purple-circle">
+            <img className="icon" src={CardIcon} alt="credit-card-icon" />
+          </div>
+        </div>
+        <div className="text-container">
+          <p className="text">{isLoading ? "Loading..." : "Buy"}</p>
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default Checkout;
